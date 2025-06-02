ad5d1bbddea237647bed91765a184a9c3cb387a0
Alexandr <ptrv.alex23@gmail.com>
2025-06-03 01:18:44 +0300

    changes

--- auth.py
+++ auth.py
@@ -2,18 +2,13 @@ import streamlit_authenticator as stauth
 import streamlit as st
 
 def get_authenticator():
-    credentials = {
-        "usernames": dict(st.secrets['credentials']['usernames'])
-    }
-    cookie_name = st.secrets["cookie"]["name"]
-    cookie_key = st.secrets["cookie"]["key"]
-    expiry_days = st.secrets["cookie"]["expiry_days"]
 
     authenticator = stauth.Authenticate(
-        credentials,  # <- обычный словарь
-        cookie_name,
-        cookie_key,
-        expiry_days
+        dict(st.secrets['credentials']),
+        st.secrets['cookie']['name'],
+        st.secrets['cookie']['key'],
+        st.secrets['cookie']['expiry_days'],
+        st.secrets['preauthorized']
     )
 
     return authenticator
