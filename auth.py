3110cd27ba6a4f72b0be71dd37f4c9170adec7c0
Alexandr <ptrv.alex23@gmail.com>
2025-06-02 23:45:55 +0300

    changes

--- auth.py
+++ auth.py
@@ -1,19 +1,15 @@
-import yaml
+import streamlit as st
 import streamlit_authenticator as stauth
 from yaml.loader import SafeLoader
 
 def load_authenticator():
-    """
-    Загружает конфиг из users.yaml и создаёт объект Authenticate.
-    Возвращает: stauth.Authenticate
-    """
-    with open("users.yaml", "r", encoding="utf-8") as file:
-        config = yaml.load(file, Loader=SafeLoader)
+    credentials = st.secrets["credentials"]
+    cookie = st.secrets["auth"]
 
     authenticator = stauth.Authenticate(
-        config["credentials"],
-        config["cookie"]["name"],
-        config["cookie"]["key"],
-        config["cookie"]["expiry_days"]
+        credentials=credentials,
+        cookie_name=cookie["cookie_name"],
+        key=cookie["cookie_key"],
+        cookie_expiry_days=cookie["cookie_expiry_days"]
     )
     return authenticator
