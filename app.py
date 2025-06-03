6e1d05878f7d7685fc55c31aedaf43dca91ce1f9
Aleksander <ptrv.alex23@gmail.com>
2025-06-03 16:36:18 +0300

    Revert "changes"

--- app.py
+++ app.py
@@ -2,51 +2,52 @@ import streamlit as st
 from account import show_login
 from json_utils import count_root_objects
 import codecs
+import pyautogui
 import json
 # from db_utils import get_db_session
 
 def run_json_tool():
     st.set_page_config(page_title="Streamlit JSON Validator", layout='wide')
 
-    if "input_json" not in st.session_state:
-        st.session_state.input_json = '[\n\t{\n\n\t}\n]'
+    c_1, c_2 = st.columns([1, 1])
 
-    col1, col2 = st.columns([1, 1])
+    with c_1:
+        reload = st.button("RELOAD")
 
-    with col1:
-        if st.button("RELOAD"):
-            st.rerun()  # безопасный перезапуск
+    with c_2:
+        clear = st.button("CLEAR")
 
-    with col2:
-        if st.button("CLEAR"):
-            st.session_state.input_json = '[\n\t{\n\n\t}\n]'
+    def Clear():
+        pyautogui.press("tab", interval=0.15)
+        pyautogui.press("tab", interval=0.15)
+        pyautogui.hotkey("ctrl", "a", 'del', interval=0.15)
 
-    left, right = st.columns([1, 1])
+    def refresh():
+        pyautogui.press("f5", interval=0.15)
 
-    with left:
-        st.session_state.input_json = st.text_area(
-            "JSON Editor",
-            st.session_state.input_json,
-            height=600,
-            key="json_input_area"
-        )
+    # Clear I / referesh
+    if clear: Clear()
+    if reload: refresh()
 
-    with right:
+    col_1, col_2 = st.columns([1, 1])
+
+    with col_1:
+        input = st.text_area("JSON Editor", "[" + "\n" + chr(9) + "{" + "\n\n" + chr(9) + "}" + "\n" + "]", height=600)
+
+    with col_2:
         try:
-            input_str = json.loads(st.session_state.input_json)
+            input_str = json.loads(input)
+            f = codecs.open("output.json", "w", encoding="utf-8")
             json_out = json.dumps(input_str, sort_keys=False, indent=4)
-
-            # Сохраняем в файл, если нужно
-            with codecs.open("output.json", "w", encoding="utf-8") as f:
-                f.write(json_out)
-
+            f.write(json_out)
+            f.flush()
+            o = open("output.json")
+            ojson = o.read()
             count = count_root_objects(input_str)
             st.success(f"Корневых объектов: {count}")
-            st.text_area("JSON OUTPUT", json_out, height=600)
-
+            st.text_area("JSON OUTPUT", ojson, height=600)
         except Exception as e:
-            st.error("Ошибка разбора JSON")
-            st.text_area("JSON OUTPUT", str(e), height=600)
+            st.text_area("JSON OUTPUT", e, height=600)
 
 def run_db_tool():
     st.header("Работа с БД (пока заглушка)")
