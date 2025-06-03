b53fb21d7172da3a7ab3cbd24ccbc7770383709c
Alexandr <ptrv.alex23@gmail.com>
2025-06-03 19:04:15 +0300

    начало для создания SQL

--- db_utils.py
+++ db_utils.py
@@ -10,13 +10,56 @@ def run_db_tool():
     st.header("Работа с БД")
 
     with st.expander("История отпусков пользователя"):
+        # Выбор метода фильтрации
+        search_by = st.radio("Фильтровать по:", ["u.league_guid", "u.id"])
+
+        # Поле ввода в зависимости от выбора
+        if search_by == "u.league_guid":
+            search_value = st.text_input("Введи значение league_guid", key="league_guid")
+        else:
+            search_value = st.text_input("Введи значение id", key="user_id")
+
+        # Список доступных полей
+        va_fields = ["va.id", "va.user_id", "va.rev", "va.start_date", "va.end_date", "va.vacation_type", "va.vacation_status"]
+        r_fields = ["r.rev", "r.timestamp", "r.creator"]
+
+        selected_va_fields = st.multiselect("Поля из vacation_aud (va):", va_fields, default=va_fields)
+        selected_r_fields = st.multiselect("Поля из revinfo (r):", r_fields, default=r_fields)
+
+        make_sql_btn = st.button("Сформировать SQL")
+
+        if make_sql_btn:
+            if not search_value:
+                st.warning(f"Введи значение для {search_by}")
+            elif search_by == "u.league_guid" and not is_valid_guid(search_value):
+                st.error("Некорректный формат GUID")
+            else:
+                selected_fields = selected_va_fields + selected_r_fields
+                if not selected_fields:
+                    st.warning("Выбери хотя бы одно поле для SELECT")
+                else:
+                    fields_str = ", ".join(selected_fields)
+                    # Экранируем значение, если нужно
+                    value_str = f"'{search_value}'" if search_by == "u.league_guid" else search_value
+
+                    sql_query = f"""
+    SELECT {fields_str}
+    FROM users u
+    JOIN vacation_aud va ON va.user_id = u.id
+    JOIN revinfo r ON va.rev = r.rev
+    WHERE {search_by} = {value_str} and u.active = true
+    """.strip()
+                    st.code(sql_query, language='sql')
+
+    with st.expander("История отпусков пользователя"):
         league_guid = st.text_input(
             "Лига гуид пользователя",
             key="league_guid"
         )
 
         # Список доступных полей
-        va_fields = ["va.id", "va.user_id", "va.rev", "va.start_date", "va.end_date", "va.vacation_type", "va.vacation_status"]
+        va_fields = ["va.id", "va.user_id", "va.rev", "va.start_date", "va.end_date", "va.vacation_type",
+                     "va.vacation_status"]
         r_fields = ["r.rev", "r.timestamp", "r.creator"]
 
         selected_va_fields = st.multiselect("Поля из vacation_aud (va):", va_fields, default=va_fields)
@@ -36,10 +79,10 @@ def run_db_tool():
                 else:
                     fields_str = ", ".join(selected_fields)
                     sql_query = f"""
-    SELECT {fields_str}
-    FROM users u
-    JOIN vacation_aud va ON va.user_id = u.id
-    JOIN revinfo r ON va.rev = r.rev
-    WHERE u.league_guid = '{league_guid}'
-    """.strip()
+       SELECT {fields_str}
+       FROM users u
+       JOIN vacation_aud va ON va.user_id = u.id
+       JOIN revinfo r ON va.rev = r.rev
+       WHERE u.league_guid = '{league_guid}'
+       """.strip()
                     st.code(sql_query, language='sql')
