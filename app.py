af4a9ba9188c85377a3958d6c3ccf2b9863708a1
Alexandr <ptrv.alex23@gmail.com>
2025-06-03 17:53:25 +0300

    подпункты для БД

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def main():
         st.stop()
 
     st.sidebar.title("Навигация")
-    choice = st.sidebar.selectbox("Выберите инструмент:", ["Проверка JSON", "Работа с БД"], disabled=True)
+    choice = st.sidebar.selectbox("Выберите инструмент:", ["Проверка JSON", "Работа с БД"])
 
     if choice == "Проверка JSON":
         run_json_tool()
