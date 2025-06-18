dcc0e3fea9fabae78ba3961edb243b9103debc15
Alexandr <ptrv.alex23@gmail.com>
2025-06-18 20:26:02 +0300

    изменена аутентификация

--- auth.py
+++ auth.py
@@ -1,25 +1,23 @@
 import pyrebase
 import streamlit as st
 
-
+# Инициализация Firebase
 cred_dict = dict(st.secrets["firebaseConfig"])
-
 firebase = pyrebase.initialize_app(cred_dict)
-auth=firebase.auth()
+auth = firebase.auth()
 
 def login(email, password):
     try:
-        login = auth.sign_in_with_email_and_password(email, password)
-        return login
-    except:
-        print("Login failed")
+        user = auth.sign_in_with_email_and_password(email, password)
+        return user  # возвращаем user-объект с токеном
+    except Exception as e:
+        print("Login failed:", e)
         return None
 
 def signup(email, password):
     try:
         user = auth.create_user_with_email_and_password(email, password)
         return user
-    except:
-        print("Email or password is incorrect")
+    except Exception as e:
+        print("Signup failed:", e)
         return None
-
--- ui/auth_ui.py
+++ ui/auth_ui.py
@@ -5,24 +5,15 @@ def show_login(cookies):
     st.title("Авторизация")
 
     with st.form(key="login_form"):
-        user_email = st.text_input(
-            "Почта",
-            key="login_user_email",
-            autocomplete="username"
-        )
-        password = st.text_input(
-            "Пароль",
-            type="password",
-            key="password",
-            autocomplete="current-password"
-        )
+        user_email = st.text_input("Почта", key="login_user_email", autocomplete="username")
+        password = st.text_input("Пароль", type="password", key="password", autocomplete="current-password")
         submit = st.form_submit_button("Войти")
 
-        if submit and login(user_email, password):
+        user = login(user_email, password)
+        if submit and user:
             st.session_state.authenticated = True
             st.session_state.username = user_email
 
-            # Куки на 7 дней
             cookies["username"] = user_email
             cookies["auth"] = "true"
             cookies.save()
@@ -31,27 +22,18 @@ def show_login(cookies):
             st.error("Неверные данные")
 
     with st.form(key="register_form"):
-        user_email = st.text_input(
-            "Почта",
-            key="register_user_email",
-            autocomplete="username"
-        )
-        password = st.text_input(
-            "Пароль",
-            type="password",
-            key="register_password",
-            autocomplete="current-password"
-        )
+        user_email = st.text_input("Почта", key="register_user_email", autocomplete="username")
+        password = st.text_input("Пароль", type="password", key="register_password", autocomplete="current-password")
         submit = st.form_submit_button("Регистрация")
 
-        if submit and signup(user_email, password):
+        user = signup(user_email, password)
+        if submit and user:
             st.session_state.authenticated = True
             st.session_state.username = user_email
 
-            # Куки на 7 дней
             cookies["username"] = user_email
             cookies["auth"] = "true"
             cookies.save()
             st.rerun()
         elif submit:
-            st.error("Неверные данные")
+            st.error("Ошибка регистрации")
