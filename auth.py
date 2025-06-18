85b9ba8e142ffa1a88678e7f092160c3448854a1
Alexandr <ptrv.alex23@gmail.com>
2025-06-18 20:17:44 +0300

    изменена аутентификация

--- auth.py
+++ auth.py
@@ -10,14 +10,16 @@ auth=firebase.auth()
 def login(email, password):
     try:
         login = auth.sign_in_with_email_and_password(email, password)
+        return login
     except:
         print("Login failed")
-    return
+        return None
 
 def signup(email, password):
     try:
         user = auth.create_user_with_email_and_password(email, password)
+        return user
     except:
         print("Email or password is incorrect")
-    return
+        return None
 
