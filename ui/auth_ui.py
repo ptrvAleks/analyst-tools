64038dc37397cdc6d60229e91ce92a0d7ea3d768
Alexandr <ptrv.alex23@gmail.com>
2025-06-18 20:33:14 +0300

    изменена аутентификация

--- ui/auth_ui.py
+++ ui/auth_ui.py
@@ -22,19 +22,19 @@ def show_login(cookies):
         elif submit:
             st.error("Неверные данные")
 
-    with st.form(key="register_form"):
-        user_email = st.text_input("Почта", key="register_user_email", autocomplete="username")
-        password = st.text_input("Пароль", type="password", key="register_password", autocomplete="current-password")
-        submit = st.form_submit_button("Регистрация")
-
-        user = signup(user_email, password)
-        if submit and user:
-            st.session_state.authenticated = True
-            st.session_state.username = user_email
-
-            cookies["username"] = user_email
-            cookies["auth"] = "true"
-            cookies.save()
-            st.rerun()
-        elif submit:
-            st.error("Ошибка регистрации")
+    # with st.form(key="register_form"):
+    #     user_email = st.text_input("Почта", key="register_user_email", autocomplete="username")
+    #     password = st.text_input("Пароль", type="password", key="register_password", autocomplete="current-password")
+    #     submit = st.form_submit_button("Регистрация")
+    #
+    #     user = signup(user_email, password)
+    #     if submit and user:
+    #         st.session_state.authenticated = True
+    #         st.session_state.username = user_email
+    #
+    #         cookies["username"] = user_email
+    #         cookies["auth"] = "true"
+    #         cookies.save()
+    #         st.rerun()
+    #     elif submit:
+    #         st.error("Ошибка регистрации")
