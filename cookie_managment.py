c824aeba22632f94237d144ac897ec3b50b7f89f
Alexandr <ptrv.alex23@gmail.com>
2025-06-18 23:57:27 +0300

    улучшены куки

--- cookie_managment.py
+++ cookie_managment.py
@@ -7,7 +7,5 @@ def set_uid_cookie(uid: str):
     cookie_manager.set("uid", uid, key="set_uid", expires_at=datetime.datetime.now() + datetime.timedelta(days=7))
 
 def get_uid_cookie():
-    result = cookie_manager.get("uid")
-    if result and result["uid"]:
-        return result["uid"]
-    return None
+    uid = cookie_manager.get("uid")
+    return uid if uid else None
