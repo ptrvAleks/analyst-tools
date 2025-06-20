38248744ab8309db46bc5e0d7cbf2a28b5c6a050
Alexandr <ptrv.alex23@gmail.com>
2025-06-20 19:34:02 +0300

    рефакторинг авторизации

--- authentication/auth_ui.py
+++ authentication/auth_ui.py
@@ -1,65 +1,28 @@
+# auth_ui.py
+from authentication.auth_manager import AuthManager
 import streamlit as st
-from authentication.auth import login, signup
-from cookie_firebase_uid import set_uid_cookie
 
-def show_login(cookies):
+def show_login(auth: AuthManager):
     st.title("Авторизация")
-    if "login_user_email" not in st.session_state:
-        st.session_state["login_user_email"] = ""
 
-    if "password" not in st.session_state:
-        st.session_state["password"] = ""
-
-    # ---------- Форма логина ----------
     with st.form(key="login_form"):
-        login_email = st.text_input("Почта", key="login_user_email",
-                                    autocomplete="username")
-        login_pwd = st.text_input("Пароль", type="password", key="password",
-                                  autocomplete="current-password")
-        login_submit = st.form_submit_button("Войти")
-
-        if login_submit:
-            user = login(login_email, login_pwd)
-            if user:
-                uid = user["localId"]
-                st.session_state.update({
-                    "authenticated": True,
-                    "username": login_email,
-                    "uid": uid,
-                })
-                set_uid_cookie(uid)
-                cookies["username"] = login_email
-                cookies["auth"] = "true"
-                cookies["uid"] = uid
-                cookies.save()
-                st.rerun()
-            else:
+        email = st.text_input("Почта", key="login_user_email", autocomplete="username")
+        pwd = st.text_input("Пароль", type="password", key="login_password", autocomplete="current-password")
+        submit = st.form_submit_button("Войти")
+        if submit:
+            if not email:
+                st.error("Введите почту")
+            elif not pwd:
+                st.error("Введите пароль")
+            elif not auth.login(email, pwd):
                 st.error("Неверные данные")
 
-    # ---------- Форма регистрации ----------
-    st.title("Регистрация")
-    with st.form(key="register_form"):
-        reg_email = st.text_input("Почта", key="register_user_email",
-                                  autocomplete="username")
-        reg_pwd = st.text_input("Пароль", type="password",
-                                key="register_password",
-                                autocomplete="new-password")
-        reg_submit = st.form_submit_button("Регистрация")
+    st.subheader("Регистрация")
+    reg_email = st.text_input("Почта", key="register_user_email", autocomplete="username")
+    reg_pwd   = st.text_input("Пароль", type="password", key="register_password", autocomplete="new-password")
 
-        if reg_submit:
-            new_user = signup(reg_email, reg_pwd)
-            if new_user:
-                uid = new_user["localId"]
-                st.session_state.update({
-                    "authenticated": True,
-                    "username": reg_email,
-                    "uid": uid,
-                })
-                set_uid_cookie(uid)
-                cookies["username"] = reg_email
-                cookies["auth"] = "true"
-                cookies["uid"] = uid
-                cookies.save()
-                st.rerun()
-            else:
-                st.error("Ошибка регистрации")
+    if st.button("Регистрация"):
+        if not auth.register(reg_email, reg_pwd):
+            st.error("Ошибка регистрации")
+        else:
+            st.success("Пользователь зарегистрирован")
