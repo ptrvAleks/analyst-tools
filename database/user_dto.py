efaace23082f26b3115324a616219b1cd30cbe94
aleksandrpetrov <petrv.alex@outlook.com>
2025-11-14 23:05:50 +0300

    исправление логина

--- database/user_dto.py
+++ database/user_dto.py
@@ -1,5 +1,7 @@
+from typing import Optional
+
 class UserDto:
-    def __init__(self, uid: str, email: str, first_name: str | None, role: str):
+    def __init__(self, uid: str, email: str, first_name: Optional[str] = None, role: str = "user"):
         self.uid = uid
         self.email = email
         self.first_name = first_name
@@ -15,19 +17,13 @@ class UserDto:
 
     @classmethod
     def from_dict(cls, uid: str, data: dict):
-        email=data.get("email")
-        role=data.get("role")
-        
+        email = data.get("email")
+        role = data.get("role", "user")  # по умолчанию user
         if email is None:
             raise ValueError("Empty email")
-        
-        if role is None:
-            raise ValueError("Empty email")
-        
         return cls(
             uid=uid,
             email=email,
             first_name=data.get("first_name"),
             role=role
         )
-    
