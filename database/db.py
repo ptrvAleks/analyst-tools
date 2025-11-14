4aaebcdbe1eb76951723834901d0a0679cd5a627
aleksandrpetrov <petrv.alex@outlook.com>
2025-11-14 22:52:18 +0300

    Изменена инициализация firebase

--- database/db.py
+++ database/db.py
@@ -4,8 +4,11 @@ from database.config import get_firebase_config, get_environment
 
 env = get_environment()
 cred = credentials.Certificate(get_firebase_config(env))
-firebase_admin.initialize_app(cred)
+
+# Проверяем, инициализирован ли уже Firebase
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-print(f"✅ Firebase подключен — среда: {get_environment()}")
+print(f"✅ Firebase подключен — среда: {env}")
